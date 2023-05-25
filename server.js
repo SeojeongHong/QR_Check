a4ec1dca48b101c4fb640fb13d01bcd84a9c57b4
홍서정 <94052799+hsj517@users.noreply.github.com>
2023-05-25 23:47:32 +0900

    feat : 과목번호로 페이지 접근

--- server.js
+++ server.js
@@ -3,20 +3,14 @@ const mysql = require('mysql2');
 var app = express();
 const hostname = 'localhost';
 const port = 3000;
-
+var connection=null;
 app.use(express.static(__dirname));
-
-app.get('/', async (req, res) =>{
-  res.sendFile(__dirname + '/QR_Check.html');
-});
-
 
 var server = app.listen(port, hostname, async () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-
   try {
     // Oracle DB 정보
-    const connection = mysql.createConnection({
+    connection = mysql.createConnection({
       host: 'localhost',
       user: 'test',
       password: '1234',
@@ -25,10 +19,41 @@ var server = app.listen(port, hostname, async () => {
 
     console.log('DB 연결 성공');
 
+
   } catch (err) {
     console.error('DB 연결 오류:', err);
   }
 });
+
+//과목번호(학수번호) -> url
+var coureid;
+
+app.get('/:id', async (req, res) =>{
+  courseid = req.params.id;
+
+  //과목번호(c_num)으로 과목 이름(c_name) 받아오는 쿼리
+  const query = 'SELECT c_name FROM course WHERE c_num = ?';
+  connection.query(query, [courseid], (err, results) => {
+    if (err) {
+      console.error('Error executing query:', err);
+      res.status(500).send('서버 오류');
+      return;
+    }
+
+    const courseName = results[0].c_name;
+
+    if (courseName) {
+      res.sendFile(__dirname + '/QR_Check.html');
+      console.log(`과목: ${courseName}`);
+    } else {
+      res.status(404).send('과목을 찾을 수 없습니다.');
+    }
+  });
+
+});
+
+
+
 
 // 서버 종료 핸들러
 process.on('SIGINT', async function () {
